bb83f6b639a2988abf4b4a731798deeea6edb498
shatko <mladen.salaj@gmail.com>
2019-03-29 13:09:28 +0100

    initial

---
+++ site/web/app/themes/shatko/resources/assets/scripts/routes/home.js
@@ -0,0 +1,9 @@
+export default {
+  init() {
+    // JavaScript to be fired on the home page
+    console.log('test3');
+  },
+  finalize() {
+    // JavaScript to be fired on the home page, after the init JS
+  },
+};
